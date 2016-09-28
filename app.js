5200226b32c88ed71b6a92a759a3b4518b40f8a0
mishaetaya <mike.kaplun@gmail.com>
2016-09-27 20:06:29 -0400

    front page working

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var express = require( 'express' );
+var nunjucks = require('nunjucks');
 var app = express(); 
+var routes = require('./routes/');
+app.use('/', routes);
 
+nunjucks.configure('views', { noCache: true }); // point nunjucks to the proper directory for templates
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // 
 
 
 app.use('/', function(req, res, next) {
@@ -18,20 +24,13 @@ app.use('/', function(req, res, next) {
 
 );
 
-
-app.get('/', function(request, response){
-
-	response.send('here is your response');
-
-});
-
-app.post('/', function(request, response){
-	request
-	response.send(status);
-});
+app.use(express.static('public'));
 
 
 
 app.listen('3000', function(){
 	console.log('listening');
 })
+
+
+
